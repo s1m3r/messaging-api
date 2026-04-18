9a7238a52b872888e60d9cec9291eae796108021
sasha <dsunndusn@g,ail.cim>
2026-04-18 14:42:49 +0300

    Initialize Spring Boot project for messaging API project

---
+++ messaging-api/src/test/java/messaging/api/MessagingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package messaging.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
